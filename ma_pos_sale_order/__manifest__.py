e8d20f3b04c60719e34f81547f85383fc3b90405
mediterraneanapps <58656212+mediterraneanapps@users.noreply.github.com>
2020-01-20 08:54:43 +0100

    Update __manifest__.py

--- ma_pos_sale_order/__manifest__.py
+++ ma_pos_sale_order/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Sale orders in POS',
     'version': '1.1.0',
-    'author': 'IT-Projects LLC, Ivan Yelizariev',
+    'author': 'Mediterranean Apps',
     'license': 'LGPL-3',
     'category': 'Point of Sale',
     "support": "mediterranean.apps@gmail.com",
