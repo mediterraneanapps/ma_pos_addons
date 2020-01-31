dd8fd84b6dfbe9097b5ba8d1bea37697f54e094f
mediterraneanapps <58656212+mediterraneanapps@users.noreply.github.com>
2020-01-31 16:05:06 +0100

    Update __manifest__.py

--- ma_pos_fiscal_current/__manifest__.py
+++ ma_pos_fiscal_current/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'POS fiscal current',
     'version': '10.0.1.0.0',
-    'author': 'IT-Projects LLC',
+    'author': 'Mediterranean Apps',
     'license': 'LGPL-3',
     'category': 'Point Of Sale',
     "support": "mediterranean.apps@gmail.com",
